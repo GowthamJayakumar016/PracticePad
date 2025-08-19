15d56ad12e61720154b8c957ac151061395cfc49
Gowtham <gowtham16124@gmail.com>
2025-08-19 20:05:33 +0530

    Dry Run

---
+++ Permutations.java
@@ -0,0 +1,23 @@
+public class Permutations {
+    public static void main(String args[])
+    {
+
+        Permutations("", "abc");
+    }
+    public static void Permutations(String p, String up)
+    {
+        if(up.isEmpty())
+        {
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        for(int i=0;i<=p.length();i++)
+        {
+            String f=up.substring(0,i);
+            String s=up.substring(i, p.length());
+
+            Permutations(f+ch+s,up.substring(1));
+        }
+    }
+}
