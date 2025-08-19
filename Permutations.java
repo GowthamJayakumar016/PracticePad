ddf27222860c1aa7026fe5f0d89ccc6d36f0413f
GowthamJayakumar016 <gowthamjayakumar016@gmail.com>
2025-08-19 20:29:44 +0530

    return type added

--- Permutations.java
+++ Permutations.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String args[])
     {
 
-        Permutations("", "abc");
+      System.out.println(PermutationsArrayList("", "abc"));
     }
     public static void Permutations(String p, String up)
     {
@@ -19,5 +21,27 @@ public class Permutations {
 
             Permutations(f+ch+s,up.substring(1));
         }
+    }
+
+    public static ArrayList<String> PermutationsArrayList(String p, String up)
+    {
+        if(up.isEmpty())
+        {
+            ArrayList<String> a=new ArrayList<>();
+           a.add(p);
+            return a ;
+        }
+        char ch=up.charAt(0);
+        ArrayList<String> list=new ArrayList<>();
+        for(int i=0;i<=p.length();i++)
+        {
+            String f=p.substring(0,i);
+            String s=p.substring(i, p.length());
+
+            ArrayList <String> ansfromabove=PermutationsArrayList(f+ch+s,up.substring(1));
+
+            list.addAll(ansfromabove);
+        }
+        return list;
     }
 }
